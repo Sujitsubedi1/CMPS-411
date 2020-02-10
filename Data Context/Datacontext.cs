90acd4e664b87308c7c82fecaa11d4ca181e8f42
LAPTOP-Q0L54CT7\Sugyan Subedi <sujit.subedi@selu.edu>
2020-02-10 08:46:55 -0600

    Initial DbContext

---
+++ Data Context/Datacontext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectInfo.Model;
+
+namespace ProjectInfo.Data_Context
+{
+    public class Datacontext: DbContext
+    {
+        public DbSet<Dummydata> DummyData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLOCALDB;Database=CMPS411;Trusted_Connection=True;");
+            }
+        }
+
+    }
+
+    
+}
