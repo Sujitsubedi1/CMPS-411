ef361ad62d76f39145ba8cea011a57924451561a
DESKTOP-9TQF49K\Abhishek <abhishek.karanjit@selu.edu>
2020-03-10 12:25:03 -0500

    ClassInfo

--- Model/ClassInfo.cs
+++ Model/ClassInfo.cs
@@ -15,9 +15,9 @@ namespace ProjectInfo.Model
 
         public string Year { get; set; }
 
-       // public ICollection<AdminData> Student { get; set; }
+        // public ICollection<AdminData> Student { get; set; }
 
-        
+
 
     }
 }
